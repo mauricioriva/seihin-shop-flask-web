9193360b745667dd98b0d88089b594fe3c9656d4
Jorge Aguilar <jorgewhite@Jorges-MacBook-Air.local>
2021-08-09 14:48:20 -0500

    Connected purchase

--- SeihinShop/routes/purchase.py
+++ SeihinShop/routes/purchase.py
@@ -1,4 +1,4 @@
-from flask import request, Blueprint, render_template
+from flask import request, Blueprint, render_template, session
 from flask.helpers import url_for
 from werkzeug.utils import redirect
 from ..db import get_db
@@ -7,16 +7,18 @@ purchase_bp = Blueprint('purchase_page', __name__, template_folder='./../templat
 
 # GET products purchased by user (obtienes la lista de productos comprados) (Devuelve un html)
 # HINT: return render_template('your_view.html', your_list=your_list)
-@purchase_bp.route('/users/<user_id>/purchases', methods=['GET'])
-def purchases(user_id):
+@purchase_bp.route('/users/purchases', methods=['GET'])
+def purchases():
+    user_id = session.get('user_id')
     name = get_db().execute(f'SELECT name FROM user WHERE user.id = {user_id}').fetchall()
     purchases = get_db().execute(
         f'SELECT p.name, p.price, pur.id, pur.date FROM product p LEFT JOIN purchase AS pur ON pur.product_id = p.id WHERE pur.user_id = {user_id}').fetchall()
     return render_template('purchase/list-purchases.html', purchases=purchases, name=name)
-
+    
 # POST purchase, un usuario compra un producto
-@purchase_bp.route('/products/<product_id>/users/<user_id>/purchase', methods=['POST', 'GET'])
-def user_purchase_product(product_id, user_id):
+@purchase_bp.route('/products/<product_id>/users/purchase', methods=['POST', 'GET'])
+def user_purchase_product(product_id):
+    user_id = session.get('user_id')
     error = None
     if request.method == 'POST':
         payment_selected = request.form['payment']
