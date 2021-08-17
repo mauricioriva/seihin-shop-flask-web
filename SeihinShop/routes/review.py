7e0b71aefb6ab9c3271bc7255132cd932d81852d
MarthaPR <marthapr@ciencias.unam.mx>
2021-08-16 21:15:35 -0500

    CR reseñas plantillas y funciones

--- SeihinShop/routes/review.py
+++ SeihinShop/routes/review.py
@@ -8,24 +8,30 @@ review_bp = Blueprint('reviews_page', __name__, template_folder='templates')
 # HINT: return render_template('your_view.html', your_list=your_list)
 @review_bp.route('/products/<product_id>/reviews', methods=['GET', 'POST'])
 def reviews(product_id):
-    if request.method == 'GET':
-        reviews = get_db().execute(
-            'SELECT * FROM review'
-        ).fetchall()
-        return render_template('review.html', reviews = reviews)
-    else:
-        pass
+    product = get_db().execute(
+        'SELECT * FROM review WHERE product_id=?', (product_id)
+    ).fetchall()
+    return render_template('list_reviews', product=product)
 
 # Crea una resena sobre el producto
 def create_review(req, product_id):
-    pass
+    description = req.form['description']
+    score = int(req.form['score'])
+    get_db().execute('INSERT INTO review (description, score) VALUES (?, ?)', (description, score))
+    get_db().commit()
+    return redirect(url_for('reviews_page.reviews'))
 
 # Devuelve la lista de resenas del producto (Devuelve HTML)
 def list_reviews(req, product_id):
-
-    return render_template('list_reviews.html')
+    if request.method == 'GET':
+        return render_template('list_reviews.html')
+    else:
+        return create_product(request)
 
 # Devuelve plantilla HTML para crear nueva resena sobre ese producto
 @review_bp.route('/products/<product_id>/reviews/new', methods=['GET'])
 def new_review(product_id):
-    return render_template('new_review.html', review = review)
+    if request.method == 'GET':
+        return render_template('new_review.html')
+    else:
+        return create_product(request)
